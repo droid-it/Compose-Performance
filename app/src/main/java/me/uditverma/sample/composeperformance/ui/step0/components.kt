862bcf42bf0e2acb6e50ae87866556daada44140
Udit Verma <udverma@expediagroup.com>
2023-04-29 23:50:25 +0530

    break into steps

--- app/src/main/java/me/uditverma/sample/composeperformance/ui/step0/components.kt
+++ app/src/main/java/me/uditverma/sample/composeperformance/ui/step0/components.kt
@@ -1,14 +1,17 @@
-package me.uditverma.sample.composeperformance.ui
+package me.uditverma.sample.composeperformance.ui.step0
 
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -22,21 +25,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
+import me.uditverma.sample.composeperformance.LogFilter
+import me.uditverma.sample.composeperformance.Logger
+import me.uditverma.sample.composeperformance.ui.Item
 import java.util.Random
+import kotlin.math.absoluteValue
 
 @Composable
 fun ComposePerformanceScreen() {
+    Logger.d(
+        message = "Recomposing entire Screen",
+        filter = LogFilter.Recomposition
+    )
+    Logger.d(
+        message = "Recreating item list",
+        filter = LogFilter.ReAllocation
+    )
     val random = Random(10)
     val items = IntRange(0, 100).map {
-        val randomNumber = random.nextInt()
-        "($randomNumber) Item number $it"
+        val randomNumber = random.nextInt().absoluteValue % 200
+        Item(
+            desc = "[$randomNumber] Item with index = $it",
+            id = randomNumber
+        )
     }
     val scrollState = rememberScrollState()
     val scope = rememberCoroutineScope()
+    Logger.d(
+        message = "Recalculating scroll progress",
+        filter = LogFilter.ReAllocation
+    )
     val scrollProgress = scrollState.value / (scrollState.maxValue * 1f)
+    Logger.d(
+        message = "Recalculating showScrollToTopButton",
+        filter = LogFilter.ReAllocation
+    )
     val showScrollToTopButton = scrollProgress > .5
-    println("composing *ComposePerformanceScreen*")
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,12 +72,13 @@ fun ComposePerformanceScreen() {
             scrollState = scrollState,
             modifier = Modifier.weight(1f)
         )
+        Spacer(modifier = Modifier.height(16.dp))
         ScrollPositionIndicator(progress = scrollProgress)
         Box(
-            modifier = Modifier.height(64.dp)
+            modifier = Modifier
+                .height(64.dp)
                 .align(Alignment.CenterHorizontally)
         ) {
-//            ScrollProgressBar(progress = scrollProgress)
             if (showScrollToTopButton) {
                 ScrollToTopButton {
                     scope.launch {
@@ -66,20 +93,41 @@ fun ComposePerformanceScreen() {
 @Composable
 fun ItemList(
     modifier: Modifier = Modifier,
-    items: List<String>,
+    items: List<Item>,
     scrollState: ScrollState = rememberScrollState()
 ) {
+    Logger.d(
+        message = "Recomposing ItemList",
+        filter = LogFilter.Recomposition
+    )
     Column(
         modifier = modifier
-            .verticalScroll(scrollState)
+            .verticalScroll(scrollState),
     ) {
+        Logger.d(
+            message = "Sorting item list",
+            filter = LogFilter.ReAllocation
+        )
         for (item in items.sorted()) {
-            Box(
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(32.dp)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = item)
+                AsyncImage(
+                    model = item.imageUrl,
+                    contentDescription = null,
+                    modifier = Modifier.size(72.dp),
+                    onLoading = {
+                        Logger.d(
+                            filter = LogFilter.ColumnLoad,
+                            message = "image ${item.id} is loading"
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = item.desc)
             }
         }
     }
@@ -96,7 +144,19 @@ fun ScrollPositionIndicator(
             .height(32.dp)
             .background(Color.Yellow)
     ) {
+        Logger.d(
+            message = "Recomposing ScrollPositionIndicator",
+            filter = LogFilter.Recomposition
+        )
+        Logger.d(
+            message = "Recalculating progressWidth",
+            filter = LogFilter.ReAllocation
+        )
         val progressWidth = maxWidth - (8.dp)
+        Logger.d(
+            message = "Recalculating item offset",
+            filter = LogFilter.ReAllocation
+        )
         val xOffset = with(LocalDensity.current) {
             ((progressWidth - 16.dp).toPx() * progress).toDp() + 4.dp
         }
@@ -116,17 +176,6 @@ fun ScrollPositionIndicator(
         )
     }
 }
-
-//@Composable
-//fun ScrollProgressBar(
-//    modifier: Modifier = Modifier,
-//    progress: Float
-//) {
-//    LinearProgressIndicator(
-//        progress = progress,
-//        modifier = modifier
-//    )
-//}
 
 @Composable
 fun ScrollToTopButton(
